9c6b9c8ce6c63b4c23943601abac965c537d2a04
sheinakorem <79397780+sheinakorem@users.noreply.github.com>
2024-02-13 10:31:02 +0200

    Add files via upload

---
+++ indexJS.js
@@ -0,0 +1,15 @@
+
+
+//sign in and sign up buttons
+document.querySelector('.signUpBtn').addEventListener('click', function() {
+  window.location.href = 'sign in +up/signUp.html';
+});
+
+document.querySelector('.signInBtn').addEventListener('click', function() {
+  window.location.href = 'sign in +up/logIn.html';
+});
+
+  
+  
+  
+  
