2ca1482c4a7f9805251cf109dcaffa90bddbd8d0
sheina korem <korem@g.jct.ac.il>
2024-02-13 22:04:52 +0200

    --all2

--- sign in +up/logIn.js
+++ sign in +up/logIn.js
@@ -1,23 +1,38 @@
+
+
+var attempt = 3;
 function login() {
     var username = document.getElementById("loginUsername").value;
     var password = document.getElementById("loginPassword").value;
-  
-
+    
+    
     // Retrieve accounts from local storage
     var accounts = JSON.parse(localStorage.getItem("accounts")) || [];
   
     // Check if the entered credentials match any existing account
     var user = accounts.find(account => account.username === username && account.password === password );
-  
-    if (user) {
+    if ( user){//sername == accounts.username && password == accounts.password){
+        alert ("Login successfully");
         document.getElementById("loginErrorMessage").innerHTML = "";
         // Set the username in local storage for use in profile page
         localStorage.setItem("loggedInUsername", username);
         // Redirect to profile page
         window.location.href = "profile.html";
-    } else {
-        document.getElementById("loginErrorMessage").innerHTML = "Invalid username or password.";
+        return false;
     }
-
-
+    else{
+        if(attempt>0){
+        attempt --;// Decrementing by one.
+        }
+        alert("You have  "+attempt+" attempts left");
+        // Disabling fields after 3 attempts.
+        if( attempt === 0){
+        document.getElementById("loginUsername").disabled = true;
+        document.getElementById("loginPassword").disabled = true;
+        document.getElementById("submit").disabled = true;
+        document.getElementById("loginErrorMessage").innerHTML = "Invalid username or password.";
+        document.getElementById("loginForm").reset(); // Clear the form
+        return false;
+    }
+}
 }
